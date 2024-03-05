65b3e0d365e8818ee5f9a8f13ff74952fcb77d5c
Ana Caroline Vieira <sv_anacaroline@live.com>
2024-03-05 20:02:50 -0300

    feat: add quick sort alg

--- typescript/sort-select/quick-sort.ts
+++ typescript/sort-select/quick-sort.ts
@@ -1,5 +1,19 @@
 export function quickSort(arr: number[]): number[] {
-    return arr;
+    if (arr.length <= 1) return arr
+    
+    let pivot = arr[0];
+    let less: number[] = [];
+    let high: number[] = [];
+
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < pivot) {
+            less.push(arr[i]);
+        } else {
+            high.push(arr[i]);
+        }
+    }
+
+    return [...quickSort(less), pivot, ...quickSort(high)] 
 }
 
 export function quickSortPartition(arr: number[]): number[] {
