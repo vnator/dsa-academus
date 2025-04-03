2f5f1b7d93bec34955b4de82560a39d3b081579a
João Henrique Serodio <jhserodio@gmail.com>
2025-04-03 20:30:51 +0000

    Resolucao de conflito besta (gerado por vscode web)

--- java/lista 2/Pessoa/Pessoa.java
+++ java/lista 2/Pessoa/Pessoa.java
@@ -2,20 +2,6 @@ public class Pessoa {
     private String nome;
     private String telefone;
 
-<<<<<<< HEAD
-    public setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public setTelefone(String telefone) {
-        this.telefone = telefone;
-    } 
-
-    public exibeDados() {
-        System.out.println("nome: " + nome + ", telefone: " + telefone);
-    }
-}
-=======
     public void setNome(String nome) {
         this.nome = nome;
     }
@@ -28,4 +14,3 @@ public class Pessoa {
         System.out.println("nome: " + nome + ", telefone: " + telefone);
     }
 }
->>>>>>> 9771659a8f19c322bfca8be9e77e738b3d68210b
